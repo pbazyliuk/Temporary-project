b164924abde3e29b9099ac2e1afbe741cc720983
Pavlo <p.bazyliuk@gmail.com>
2017-05-23 17:44:25 +0300

    removed usersOn from state.uiState

--- client/src/app/store/ui-state.ts
+++ client/src/app/store/ui-state.ts
@@ -3,13 +3,11 @@ import { User } from 'app/core/models/user';
 export interface UiState {
     user: User;
     authenticated: boolean;
-    usersOn: Array<object>;
     currentChat: string;
 };
 
 export const INITIAL_UI_STATE: UiState = {
     user: undefined,
     authenticated: undefined,
-    currentChat: undefined,
-    usersOn: []
+    currentChat: undefined
 };
